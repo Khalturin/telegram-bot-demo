e6280de0ebf0b293a5fd3a7ba167f578a61ff9ae
Khalturin <mrjokko@gmail.com>
2022-05-04 22:49:47 +0300

    Do not do this in real project

--- internal/app/commands/commander.go
+++ internal/app/commands/commander.go
@@ -5,6 +5,8 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+var registeredCommands = map[string]func(c *Commander, msg *tgbotapi.Message){}
+
 type Commander struct {
 	bot            *tgbotapi.BotAPI
 	productService *product.Service
@@ -17,4 +19,26 @@ func NewCommander(bot *tgbotapi.BotAPI,
 		bot:            bot,
 		productService: productService,
 	}
+}
+
+func (c *Commander) HandleUpdate(update tgbotapi.Update) {
+	if update.Message == nil { // ignore any non-Message Updates
+		return
+	}
+
+	command, ok := registeredCommands[update.Message.Command()]
+	if ok {
+		command(c, update.Message)
+	} else {
+		c.Default(update.Message)
+	}
+
+	//switch update.Message.Command() {
+	//case "help":
+	//	c.Help(update.Message)
+	//case "list":
+	//	c.List(update.Message)
+	//default:
+	//	c.Default(update.Message)
+	//}
 }
